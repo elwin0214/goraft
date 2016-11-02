a520d0d801cf2b78a51816bd3faa788820e6fba0
gwei <gongwei0214@gmail.com>
2016-11-02 21:27:16 +0800

    implement logger

---
+++ src/raft/logger.go
@@ -0,0 +1,20 @@
+package raft
+
+import (
+	"log"
+	"os"
+)
+
+type Logger struct {
+	Trace *log.Logger
+	Debug *log.Logger
+	Info  *log.Logger
+	Error *log.Logger
+}
+
+func (logger *Logger) Init() {
+	logger.Trace = log.New(os.Stdout, "[TRACE]", log.LstdFlags)
+	logger.Debug = log.New(os.Stdout, "[DEBUG]", log.LstdFlags)
+	logger.Info = log.New(os.Stdout, "[INFO ]", log.LstdFlags)
+	logger.Error = log.New(os.Stdout, "[ERROR]", log.LstdFlags)
+}
